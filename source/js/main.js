313d91af6ef56539f5d60a31410f33bc6bb1a188
Каролина <carolina16_kv@mail.ru>
2022-07-25 23:01:01 +1000

    на первую проверку

---
+++ source/js/main.js
@@ -0,0 +1,72 @@
+import {iosVhFix} from './utils/ios-vh-fix';
+import {initModals} from './modules/modals/init-modals';
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  // Utils
+  // ---------------------------------
+
+  iosVhFix();
+
+  // Modules
+  // ---------------------------------
+
+  //  Открытие меню
+
+  const navMain = document.querySelector('.main-nav__menu');
+  const navToggle = document.querySelector('.main-nav__toggle');
+  const headerWrapper = document.querySelector('.page-header__wrapper');
+  const header = document.querySelector('.page-header');
+  
+  navMain.classList.remove('main-nav__menu--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav__menu--closed')) {
+    navMain.classList.remove('main-nav__menu--closed');
+    navMain.classList.add('main-nav__menu--opened');
+    headerWrapper.classList.add('page-header__wrapper--opened');
+    header.style.height='538px';
+    document.querySelector('.page-header__logo-icon').style.fill='rgba(1, 28, 64, 1)';
+    
+    } else {
+    navMain.classList.add('main-nav__menu--closed');
+    navMain.classList.remove('main-nav__menu--opened');
+    headerWrapper.classList.remove('page-header__wrapper--opened');
+    document.querySelector('.page-header__logo-icon').style.fill='rgba(249, 251, 253, 1)';
+    header.style.height='auto';
+    }
+  });
+
+
+
+  // Плавный скролл 
+
+  const siteLinks = document.querySelectorAll('a[href*="#"]');
+
+  if (siteLinks.length >= 1) { 
+
+    for (let siteLink of siteLinks) {
+      siteLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const blockID = siteLink.getAttribute('href');
+        
+        document.getElementById(blockID).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        });
+      });
+    }
+
+  };
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    initModals();
+  });
+});
+
+
